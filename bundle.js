724501664d40a8b34f94ecbc54c9e34de9f13421
hrm <50558852+hrmtk@users.noreply.github.com>
2019-06-18 15:10:43 +0900

    Add pelindrome.html

--- bundle.js
+++ bundle.js
@@ -27,13 +27,24 @@ function Phrase(content) {
 
 },{}],2:[function(require,module,exports){
 let Phrase = require("mhartl-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+function palindromeTester(event) {
+	event.preventDefault();
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+  	palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+  	palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event){
+  	palindromeTester(event);
+  });
+});
 
 },{"mhartl-palindrome":1}]},{},[2]);
